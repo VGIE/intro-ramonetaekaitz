893bd414f8dd7d4f0c8a462b08798cfdd19fdf8e
RamonyEkaitz <ramon134jimenez@gmail.com>
2025-09-22 14:39:03 +0200

    ejercicio 1

--- IntroExercises/Exercises.cs
+++ IntroExercises/Exercises.cs
@@ -12,13 +12,23 @@ namespace IntroExercises
         //Find should return the position in array where value appears for the first time. -1 if the value isn't found or the array is empty/null
         public static int Find(int[] array, int value)
         {
-            return 0;
+
+         for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == value)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
         }
 
         //TODO #2
         //Count should return the number of times value appears in array. 0 if the array is empty/null
         public static int Count(int[] array, int value)
         {
+            
             return 0;
         }
 
